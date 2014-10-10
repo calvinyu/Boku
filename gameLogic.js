f5fb99f8dbd281dbfbdc9da8678bb51647fa1efd
Calvin Yu <ycy247@nyu.edu>
2014-10-10 02:35:53 -0400

    hw5

--- gameLogic.js
+++ gameLogic.js
@@ -16,200 +16,220 @@ angular.module('myApp.gameLogic', []).service('gameLogic', function(){
  *  10           x x x x x  
  */
 
+/* Grid representation:
+ *
+ *     0 1 2 3 4 5 6 7 8 9
+ *   0       x x x 
+ *   1   x x x x x x x
+ *   2 x x x x x x x x x
+ *   3 x x x x x x x x x
+ *   4 x x x x x x x x x
+ *   5 x x x x x x x x x  
+ *   6 x x x x x x x x x 
+ *   7     x x x x x     
+ *   8         x         
+ *   9                  
+ *  10                    
+ */
+
+
 //the number of consecutive pawns to win
 var N = 5;
 //the boundary of horizontal direction
 var horIndex = [[0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [0, 10],
-	[1, 10], [2, 10], [3, 10], [4, 10], [5, 10]];
+  [1, 10], [2, 10], [3, 10], [4, 10], [5, 10]];
 //the boundary of vertical direction
 var verIndex = [[0, 6], [0, 7], [0, 8], [0, 9], [0, 10],
-	[1, 11], [2, 11], [3, 11], [4, 11], [5, 11]];
+  [1, 11], [2, 11], [3, 11], [4, 11], [5, 11]];
 //the boundary of diagonal direction
 // starting point from NW side, end at row==10 or col==9
 var tilIndex = [[0, 4], [0, 3], [0, 2], [0, 1], [0, 0], [1, 0],
-	[2, 0], [3, 0], [4, 0], [5, 0]];
+  [2, 0], [3, 0], [4, 0], [5, 0]];
 
 /*jslvar devel: true, indent: 2 */
 /*global console */
-	//Defines that JavaScript code should be executed in "strict mode".
-	'use strict';
-	function isEqual(object1, object2) {
-		return JSON.stringify(object1) === JSON.stringify(object2);
-	}
+  //Defines that JavaScript code should be executed in "strict mode".
+  'use strict';
+  function isEqual(object1, object2) {
+    return JSON.stringify(object1) === JSON.stringify(object2);
+  }
 
-	function copyObject(object) {
-		return JSON.parse(JSON.stringify(object));
-	}
+  function copyObject(object) {
+    return JSON.parse(JSON.stringify(object));
+  }
 
-	/** Return the winner (either 'R' or 'Y') or '' if there is no winner. */
-	function getWinner(board) {
-		//check left to right
-		var i, cnt, j;
-		for(i=0; i<11; ++i) {
-			cnt = 0;
-			for(j=horIndex[i][0]; j<horIndex[i][1]; ++j){
-				if(board[i][j] !== ''){
-					if(j===0 || board[i][j-1] === board[i][j]){
-						cnt++;
-					}
-					else{
-						cnt = 1;
-					}
-					if( cnt === N ){
-						return board[i][j];
-					}
-				}
-			}
-		}
+  /** Return the winner (either 'R' or 'Y') or '' if there is no winner. */
+  function getWinner(board) {
+    //check left to right
+    var i, cnt, j;
+    for(i=0; i<11; ++i) {
+      cnt = 0;
+      for(j=horIndex[i][0]; j<horIndex[i][1]; ++j){
+        if(board[i][j] !== ''){
+          if(j===0 || board[i][j-1] === board[i][j]){
+            cnt++;
+          }
+          else{
+            cnt = 1;
+          }
+          if( cnt === N ){
+            return board[i][j];
+          }
+        }
+      }
+    }
 
-		//check NE<->SW
-		for(j=0; j<10; ++j){
-			cnt = 0;
-			for(i=verIndex[j][0]; i<verIndex[j][1]; ++i){
-				if(board[i][j] !== ''){
-					if(i===0 || board[i-1][j] === board[i][j]){
-						cnt++;
-					}
-					else{
-						cnt = 1;
-					}
-					if( cnt === N ){
-						return board[i][j];
-					}
-				}
-			}
-		}
-		var row, col;
-		//check NW<->SE
-		for(i=0; i<10; ++i) {
-			cnt =0;
-			for(row = tilIndex[i][0], col = tilIndex[i][1]; row<11 && col<10; row++, col++){
-				if(board[row][col] !== ''){
-					if(row === 0 || col===0 || board[row][col]===board[row-1][col-1]){
-						cnt++;
-					}
-					else{
-						cnt = 1;
-					}
-					if(cnt === N){
-						return board[row][col];
-					}
-				}
-			}
-		}
-		return '';
-	}//Done
+    //check NE<->SW
+    for(j=0; j<10; ++j){
+      cnt = 0;
+      for(i=verIndex[j][0]; i<verIndex[j][1]; ++i){
+        if(board[i][j] !== ''){
+          if(i===0 || board[i-1][j] === board[i][j]){
+            cnt++;
+          }
+          else{
+            cnt = 1;
+          }
+          if( cnt === N ){
+            return board[i][j];
+          }
+        }
+      }
+    }
+    var row, col;
+    //check NW<->SE
+    for(i=0; i<10; ++i) {
+      cnt =0;
+      for(row = tilIndex[i][0], col = tilIndex[i][1]; row<11 && col<10; row++, col++){
+        if(board[row][col] !== ''){
+          if(row === 0 || col===0 || board[row][col]===board[row-1][col-1]){
+            cnt++;
+          }
+          else{
+            cnt = 1;
+          }
+          if(cnt === N){
+            return board[row][col];
+          }
+        }
+      }
+    }
+    return '';
+  }//Done
+  function isInsideBoard(row,col) {
+    return (row>=0 && row<=10) && (horIndex[row][0] <= col) && (col < howIndex[row][1]);
+  }
 
-	/** Returns true if the game ended in a tie because there are no empty cells. */
-	function isTie(board) {
-		var i,j;
-		for(i=0; i<11; ++i) {
-			for(j=horIndex[i][0]; j<horIndex[i][1]; ++j){
-				if(board[i][j] === ''){
-					return false;
-				}
-			}
-		}
-		return true;
-	}//Done
+  /** Returns true if the game ended in a tie because there are no empty cells. */
+  function isTie(board) {
+    var i,j;
+    for(i=0; i<11; ++i) {
+      for(j=horIndex[i][0]; j<horIndex[i][1]; ++j){
+        if(board[i][j] === ''){
+          return false;
+        }
+      }
+    }
+    return true;
+  }//Done
 
-	/** 
-	 * Returns the move that should be performed when player 
-	 * with index turnIndexBeforeMove makes a move in cell row X col. 
-	 */
-	function createMove(board, row, col, delDirRow, delDirCol, delDis, turnIndexBeforeMove) {
-		console.log(row, col);
-		if(board === undefined) board = setBoard();
-		if (board[row][col] !== '') {
-      	throw new Error("One can only make a move in an empty position!");
-    	}
-		var boardAfterMove = copyObject(board);
-		// first one should be Red
-		boardAfterMove[row][col] = turnIndexBeforeMove === 0 ? 'R' : 'Y';
-		//remove one of the opponent's pawn
-		if(delDis !== 0) {
-			boardAfterMove[row+delDirRow*delDis][col+delDirCol*delDis] = '';
-		}
-		
-		var winner = getWinner(boardAfterMove);
-		var firstOperation;
-		if (winner !== '' || isTie(boardAfterMove)) {
-			// Game over.
-			firstOperation = {endMatch: {endMatchScores: 
-				(winner === 'R' ? [1, 0] : (winner === 'Y' ? [0, 1] : [0, 0]))}};
-		} else {
-			// Game continues. Now it's the opponent's turn (the turn switches from 0 to 1 and 1 to 0).
-			firstOperation = {setTurn: {turnIndex: 1 - turnIndexBeforeMove}};
-		}
-		return [firstOperation,
-			   {set: {key: 'board', value: boardAfterMove}},
-			   {set: {key: 'delta', value: {row: row, col: col, delDirRow: delDirRow, delDirCol: delDirCol, delDis: delDis}}}];
-	}//done
+  /** 
+   * Returns the move that should be performed when player 
+   * with index turnIndexBeforeMove makes a move in cell row X col. 
+   */
+  function createMove(board, row, col, delDirRow, delDirCol, delDis, turnIndexBeforeMove) {
+    console.log(row, col);
+    if(board === undefined) board = setBoard();
+    if (board[row][col] !== '') {
+        throw new Error("One can only make a move in an empty position!");
+      }
+    var boardAfterMove = copyObject(board);
+    // first one should be Red
+    boardAfterMove[row][col] = turnIndexBeforeMove === 0 ? 'R' : 'Y';
+    //remove one of the opponent's pawn
+    if(delDis !== 0) {
+      boardAfterMove[row+delDirRow*delDis][col+delDirCol*delDis] = '';
+    }
+    
+    var winner = getWinner(boardAfterMove);
+    var firstOperation;
+    if (winner !== '' || isTie(boardAfterMove)) {
+      // Game over.
+      firstOperation = {endMatch: {endMatchScores: 
+        (winner === 'R' ? [1, 0] : (winner === 'Y' ? [0, 1] : [0, 0]))}};
+    } else {
+      // Game continues. Now it's the opponent's turn (the turn switches from 0 to 1 and 1 to 0).
+      firstOperation = {setTurn: {turnIndex: 1 - turnIndexBeforeMove}};
+    }
+    return [firstOperation,
+         {set: {key: 'board', value: boardAfterMove}},
+         {set: {key: 'delta', value: {row: row, col: col, delDirRow: delDirRow, delDirCol: delDirCol, delDis: delDis}}}];
+  }//done
 
-	function isMoveOk(params) {
-		var move = params.move; 
-		var turnIndexBeforeMove = params.turnIndexBeforeMove; 
-		var stateBeforeMove = params.stateBeforeMove; 
-		var i, j;
-		try {
-			var deltaValue = move[2].set.value;
-			var row = deltaValue.row;
-			var col = deltaValue.col;
-			// the row direction of the pawn that's going to be removed relative to row
-			// i.e. direction of (removedRow - row)
-			var delDirRow = deltaValue.delDirRow;
-			// the column direction of the pawn that's going to be removed relative to col
-			// i.e. direction of (removedColumn - column)
-			var delDirCol = deltaValue.delDirCol;
-			// the distance of the pawn that's going to be removed relative to row/column
-			// i.e. abs(removedRow - row + removedCol - col)
-			var delDis = deltaValue.delDis;
-			var board = stateBeforeMove.board;
-			var rowBeforeMove = stateBeforeMove.row;
-			var colBeforeMove = stateBeforeMove.col;
-			var delDisBeforeMove = stateBeforeMove.delDis;
-			var delDirRowBeforeMove = stateBeforeMove.delDirRow;
-			var delDirColBeforeMove = stateBeforeMove.delDirCol;
-			var delRow = rowBeforeMove + delDisBeforeMove*delDirRowBeforeMove;
-			var delCol = colBeforeMove + delDisBeforeMove*delDirColBeforeMove;
-			if (board === undefined) {
-				// Initially (at the beginning of the match), stateBeforeMove is {}. 
-				board = setBoard();
-			}
-			// One can't place at a position that was taken at last move.
-			if(delDis!==0 && delDisBeforeMove !== 0 && delDirRow === row && delDirCol === col) {
-				return false;
-			}
-			// Only when the opponents pawn is trapped can be removed.
-			if(delDis !== 0){
-				if(delDis > 2 || delDis < 0){
-					return false;
-				}
-				if(row+3*delDirRow < 0 || col+3*delDirCol < 0){
-					return false;
-				}
-				if(board[row+delDirRow][col+delDirCol] !== (turnIndexBeforeMove===0?'Y':'R') ){
-					return false;
-				}
-				if(board[row+2*delDirRow][col+2*delDirCol] !== (turnIndexBeforeMove===0?'Y':'R') ){
-					return false;
-				}
-				if(board[row+3*delDirRow][col+3*delDirCol] !== (turnIndexBeforeMove===0?'R':'Y') ){
-					return false;
-				}
-			}
-			var expectedMove = createMove(board, row, col, delDirRow, delDirCol, delDis, turnIndexBeforeMove);
-			if (!isEqual(move, expectedMove)) {
-				return false;
-			}
-		} catch (e) {
-			// if there are any exceptions then the move is illegal
-			return false;
-		}
-		return true;
-	}
-	
-	/** Returns an array of {stateBeforeMove, move, comment}. */
+  function isMoveOk(params) {
+    var move = params.move; 
+    var turnIndexBeforeMove = params.turnIndexBeforeMove; 
+    var stateBeforeMove = params.stateBeforeMove; 
+    var i, j;
+    try {
+      var deltaValue = move[2].set.value;
+      var row = deltaValue.row;
+      var col = deltaValue.col;
+      // the row direction of the pawn that's going to be removed relative to row
+      // i.e. direction of (removedRow - row)
+      var delDirRow = deltaValue.delDirRow;
+      // the column direction of the pawn that's going to be removed relative to col
+      // i.e. direction of (removedColumn - column)
+      var delDirCol = deltaValue.delDirCol;
+      // the distance of the pawn that's going to be removed relative to row/column
+      // i.e. abs(removedRow - row + removedCol - col)
+      var delDis = deltaValue.delDis;
+      var board = stateBeforeMove.board;
+      var rowBeforeMove = stateBeforeMove.row;
+      var colBeforeMove = stateBeforeMove.col;
+      var delDisBeforeMove = stateBeforeMove.delDis;
+      var delDirRowBeforeMove = stateBeforeMove.delDirRow;
+      var delDirColBeforeMove = stateBeforeMove.delDirCol;
+      var delRow = rowBeforeMove + delDisBeforeMove*delDirRowBeforeMove;
+      var delCol = colBeforeMove + delDisBeforeMove*delDirColBeforeMove;
+      if (board === undefined) {
+        // Initially (at the beginning of the match), stateBeforeMove is {}. 
+        board = setBoard();
+      }
+      // One can't place at a position that was taken at last move.
+      if(delDis!==0 && delDisBeforeMove !== 0 && delDirRow === row && delDirCol === col) {
+        return false;
+      }
+      // Only when the opponents pawn is trapped can be removed.
+      if(delDis !== 0){
+        if(delDis > 2 || delDis < 0){
+          return false;
+        }
+        if(row+3*delDirRow < 0 || col+3*delDirCol < 0){
+          return false;
+        }
+        if(board[row+delDirRow][col+delDirCol] !== (turnIndexBeforeMove===0?'Y':'R') ){
+          return false;
+        }
+        if(board[row+2*delDirRow][col+2*delDirCol] !== (turnIndexBeforeMove===0?'Y':'R') ){
+          return false;
+        }
+        if(board[row+3*delDirRow][col+3*delDirCol] !== (turnIndexBeforeMove===0?'R':'Y') ){
+          return false;
+        }
+      }
+      var expectedMove = createMove(board, row, col, delDirRow, delDirCol, delDis, turnIndexBeforeMove);
+      if (!isEqual(move, expectedMove)) {
+        return false;
+      }
+    } catch (e) {
+      // if there are any exceptions then the move is illegal
+      return false;
+    }
+    return true;
+  }
+  
+  /** Returns an array of {stateBeforeMove, move, comment}. */
   function getExampleMoves(initialTurnIndex, initialState, arrayOfRowColComment) {
     var exampleMoves = [];
     var state = initialState;
@@ -273,43 +293,45 @@ var tilIndex = [[0, 4], [0, 3], [0, 2], [0, 1], [0, 0], [1, 0],
   }
 
   
-		function setBoard(){
-			var i, j;
-			var board = new Array(11);
-			for(i=0; i<11; ++i){
-				board[i] = new Array(10);
-				for(j=horIndex[i][0]; j<horIndex[i][1]; ++j){
-					board[i][j] = '';
-				}
-			}
-			return board;
-		}
+    function setBoard(){
+      var i, j;
+      var board = new Array(11);
+      for(i=0; i<11; ++i){
+        board[i] = new Array(10);
+        for(j=horIndex[i][0]; j<horIndex[i][1]; ++j){
+          board[i][j] = '';
+        }
+      }
+      return board;
+    }
 
-		// "Manual testing" --- expected result is [true, true, false].
-			board = setBoard();
-			board[0][0] = 'R';
-			var board2 = copyObject(board);
-			board2[0][1] = 'Y';		
-		console.log(
-				[ // Check placing X in 0x0 from initial state.
-				isMoveOk({turnIndexBeforeMove: 0, stateBeforeMove: {}, 
-					move: [{setTurn: {turnIndex : 1}},
-				{set: {key: 'board', value: board}},
-				{set: {key: 'delta', value: {row: 0, col: 0, delDirRow:0, delDirCol:0, delDis:0}}}]}),
-				// Check placing O in 0x1 from previous state.   
-				isMoveOk({turnIndexBeforeMove: 1, 
-					stateBeforeMove: {board: board, delta: {row: 0, col: 0, delDirRow:0, delDirCol:0, delDis:0}}, 
-				move: [{setTurn: {turnIndex : 0}},
-				{set: {key: 'board', value: board2}},
-				{set: {key: 'delta', value: {row: 0, col: 1, delDirRow:0, delDirCol:0, delDis:0}}}]}),
-				// Checking an illegal move.
-				isMoveOk({turnIndexBeforeMove: 0, stateBeforeMove: {}, move: [{setTurn: {turnIndex : 0}}]})
-				]);
+    // "Manual testing" --- expected result is [true, true, false].
+      board = setBoard();
+      board[0][0] = 'R';
+      var board2 = copyObject(board);
+      board2[0][1] = 'Y';   
+    console.log(
+        [ // Check placing X in 0x0 from initial state.
+        isMoveOk({turnIndexBeforeMove: 0, stateBeforeMove: {}, 
+          move: [{setTurn: {turnIndex : 1}},
+        {set: {key: 'board', value: board}},
+        {set: {key: 'delta', value: {row: 0, col: 0, delDirRow:0, delDirCol:0, delDis:0}}}]}),
+        // Check placing O in 0x1 from previous state.   
+        isMoveOk({turnIndexBeforeMove: 1, 
+          stateBeforeMove: {board: board, delta: {row: 0, col: 0, delDirRow:0, delDirCol:0, delDis:0}}, 
+        move: [{setTurn: {turnIndex : 0}},
+        {set: {key: 'board', value: board2}},
+        {set: {key: 'delta', value: {row: 0, col: 1, delDirRow:0, delDirCol:0, delDis:0}}}]}),
+        // Checking an illegal move.
+        isMoveOk({turnIndexBeforeMove: 0, stateBeforeMove: {}, move: [{setTurn: {turnIndex : 0}}]})
+        ]);
 
-		this.isMoveOk=isMoveOk;
-		this.horIndex = horIndex;
-		this.copyObject = copyObject;
-		this.getExampleGame = getExampleGame;
-		this.setBoard = setBoard;
-		this.getRiddles = getRiddles;
+    this.isMoveOk=isMoveOk;
+    this.horIndex = horIndex;
+    this.copyObject = copyObject;
+    this.getExampleGame = getExampleGame;
+    this.setBoard = setBoard;
+    this.getRiddles = getRiddles;
+    this.createMove = createMove;
+    this.board = setBoard();
 });
---
+++ hexagon.js
@@ -0,0 +1,223 @@
+'use strict';
+
+angular.module('myApp.hexagon',[]).service('hexagon', function($window) {
+    // Hex math defined here: http://blog.ruslans.com/2011/02/hexagonal-grid-math.html
+    var hex = this;
+    this.HexagonGrid = function(canvasId, radius) {
+        hex.radius = radius;
+
+        hex.height = Math.sqrt(3) * radius;
+        hex.width = 2 * radius;
+        hex.side = (3 / 2) * radius;
+
+        hex.canvas = document.getElementById(canvasId);
+        hex.context = hex.canvas.getContext('2d');
+
+        hex.canvasOriginX = 0;
+        hex.canvasOriginY = 0;
+        
+    };
+
+    this.drawHexGrid = function (hor, originX, originY, isDebug) {
+        hex.canvasOriginX = originX;
+        hex.canvasOriginY = originY;
+        
+        var currentHexX;
+        var currentHexY;
+        var debugText = "";
+
+        var offsetColumn = false;
+
+        for (var col = 0; col < 12; col++) {
+            for (var row = 0; row < 12; row++) {
+                var x = parseInt((5-col+2*row)/2, 10);
+                if(5-col+2*row<0) x = 1000;
+                var y = x + col - 5;
+                if (!offsetColumn) {
+                    currentHexX = (col * hex.side) + originX;
+                    currentHexY = (row * hex.height) + originY;
+                } else {
+                    currentHexX = col * hex.side + originX;
+                    currentHexY = (row * hex.height) + originY + (hex.height * 0.5);
+                }
+
+                if (isDebug) {
+                    debugText = currentHexY;
+                }
+                console.log(x,y);
+                if(x>=0 && x<11 && hor[x][0]<=y &&  y<hor[x][1]) hex.drawHex(currentHexX, currentHexY, "#ddd", debugText);
+            }
+            offsetColumn = !offsetColumn;
+        }
+    };
+
+    this.drawHexAtColRow = function(column, row, color) {
+        var drawy = column % 2 == 0 ? (row * hex.height) + hex.canvasOriginY : (row * hex.height) + hex.canvasOriginY + (hex.height / 2);
+        var drawx = (column * hex.side) + hex.canvasOriginX;
+
+        hex.drawHex(drawx, drawy, color, "");
+    };
+
+    this.drawHex = function(x0, y0, fillColor, debugText) {
+        hex.context.strokeStyle = "#000";
+        hex.context.beginPath();
+        hex.context.moveTo(x0 + hex.width - hex.side, y0);
+        hex.context.lineTo(x0 + hex.side, y0);
+        hex.context.lineTo(x0 + hex.width, y0 + (hex.height / 2));
+        hex.context.lineTo(x0 + hex.side, y0 + hex.height);
+        hex.context.lineTo(x0 + hex.width - hex.side, y0 + hex.height);
+        hex.context.lineTo(x0, y0 + (hex.height / 2));
+
+        if (fillColor) {
+            hex.context.fillStyle = fillColor;
+            hex.context.fill();
+        }
+
+        hex.context.closePath();
+        hex.context.stroke();
+
+        if (debugText) {
+            hex.context.font = "8px";
+            hex.context.fillStyle = "#000";
+            hex.context.fillText(debugText, x0 + (hex.width / 2) - (hex.width/4), y0 + (hex.height - 5));
+        }
+    };
+
+    //Recusivly step up to the body to calculate canvas offset.
+    this.getRelativeCanvasOffset = function() {
+        //return {x:hex.canvas.getBoundingClientRect().left, y:hex.canvas.getBoundingClientRect().top};
+        var x = 0, y = 0;
+        var layoutElement = hex.canvas;
+        while (layoutElement.offsetParent) {
+            x += layoutElement.offsetLeft;
+            y += layoutElement.offsetTop;
+            layoutElement = layoutElement.offsetParent;    
+        }
+        hex.offSetX = x;
+        hex.offSetY = y;
+        return { x: x, y: y };
+        
+    }
+
+    //Uses a grid overlay algorithm to determine hexagon location
+    //Left edge of grid has a test to acuratly determin correct hex
+    this.getSelectedTile = function(mouseX, mouseY) {
+
+        var offSet = hex.getRelativeCanvasOffset();
+        console.log("mouse: ", mouseX, mouseY);
+
+        mouseX -= offSet.x;       
+        mouseY -= offSet.y;
+
+        mouseX /= hex.scale ;
+        mouseY /= hex.scale ;
+
+        mouseX -= hex.tx;
+        mouseY -= hex.ty;
+        mouseX -= hex.canvasOriginX;
+        mouseY -= hex.canvasOriginY;
+
+        var column = Math.floor((mouseX) / hex.side);
+        var row = Math.floor(
+            column % 2 == 0
+                ? Math.floor((mouseY) / hex.height)
+                : Math.floor(((mouseY + (hex.height * 0.5)) / hex.height)) - 1);
+
+
+        //Test if on left side of frame            
+        if (mouseX > (column * hex.side) && mouseX < (column * hex.side) + hex.width - hex.side) {
+
+
+            //Now test which of the two triangles we are in 
+            //Top left triangle points
+            var p1 = new Object();
+            p1.x = column * hex.side;
+            p1.y = column % 2 == 0
+                ? row * hex.height
+                : (row * hex.height) + (hex.height / 2);
+
+            var p2 = new Object();
+            p2.x = p1.x;
+            p2.y = p1.y + (hex.height / 2);
+
+            var p3 = new Object();
+            p3.x = p1.x + hex.width - hex.side;
+            p3.y = p1.y;
+
+            var mousePoint = new Object();
+            mousePoint.x = mouseX;
+            mousePoint.y = mouseY;
+
+            if (hex.isPointInTriangle(mousePoint, p1, p2, p3)) {
+                column--;
+
+                if (column % 2 != 0) {
+                    row--;
+                }
+            }
+
+            //Bottom left triangle points
+            var p4 = new Object();
+            p4 = p2;
+
+            var p5 = new Object();
+            p5.x = p4.x;
+            p5.y = p4.y + (hex.height / 2);
+
+            var p6 = new Object();
+            p6.x = p5.x + (hex.width - hex.side);
+            p6.y = p5.y;
+
+            if (hex.isPointInTriangle(mousePoint, p4, p5, p6)) {
+                column--;
+
+                if (column % 2 == 0) {
+                    row++;
+                }
+            }
+        }
+
+        return  { row: row, column: column };
+    };
+
+
+    this.sign = function(p1, p2, p3) {
+        return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+    };
+
+    //TODO: Replace with optimized barycentric coordinate method
+    this.isPointInTriangle = function isPointInTriangle(pt, v1, v2, v3) {
+        var b1, b2, b3;
+
+        b1 = hex.sign(pt, v1, v2) < 0.0;
+        b2 = hex.sign(pt, v2, v3) < 0.0;
+        b3 = hex.sign(pt, v3, v1) < 0.0;
+
+        return ((b1 == b2) && (b2 == b3));
+    };
+
+    this.getIndex = function (e) {
+        var mouseX = e.pageX;
+        var mouseY = e.pageY;
+
+        var tile = hex.getSelectedTile(mouseX, mouseY);
+        //reversed for display reason;
+        hex.column = tile.column;
+        hex.row = tile.row;
+    };
+    this.updateUI = function(turn){
+        if (hex.column >= 0 && hex.row >= 0) {
+            var drawy = hex.column % 2 == 0 ? (hex.row * hex.height) 
+            + hex.canvasOriginY + 6 : (hex.row * hex.height) + hex.canvasOriginY + 6 + (hex.height / 2);
+            var drawx = (hex.column * hex.side) + hex.canvasOriginX;
+            if(turn === 1)
+                hex.drawHex(drawx, drawy - 6, "rgba(255,0,0,0.4)", "");
+            else
+                hex.drawHex(drawx, drawy - 6, "rgba(255,255,0,0.4)", "");
+            
+        } 
+    };
+
+});
+
+
